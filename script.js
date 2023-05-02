6d8322dcf4e1f0b26155a453243cd945408ebe52
Ceejay-777 <covenantcrackslord01@gmail.com>
2023-05-02 13:18:43 +0100

    commit

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 document.querySelector(".hamburger").addEventListener("click", function() {
+    document.querySelector(".nav-mobile").classList.remove("disappear");
     document.querySelector(".nav-mobile").classList.remove("hidden");
-    document.querySelector(".nav-mobile").classList.add("block");
+    document.querySelector(".nav-mobile").classList.add("appear");
+});
+
+document.querySelector(".mobile-cancle").addEventListener("click", function() {
+    document.querySelector(".nav-mobile").classList.add("disappear");
 });
